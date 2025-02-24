37a372c21c8e44c4ce4f1692110801d57fe01aca
MdAsraful56 <work.mdasraful56@gmail.com>
2025-02-24 23:36:14 +0600

    you login or sign out onAuthstateChangd

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -1,26 +1,42 @@
 /* eslint-disable react-refresh/only-export-components */
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import PropTypes from 'prop-types'; // ES6
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
 import auth from "../Firebase/firebase.init";
 
 export const AuthContext = createContext();
 
 const AuthProvider = ({children}) => {
 
+    const [User, setUser] = useState(null);
+
     const name = 'Ashraful Islam';
 
     const createUser = (email, password) => {
         return createUserWithEmailAndPassword(auth, email, password)
-    }
+    };
 
     const signInUser = (email, password) => {
         return signInWithEmailAndPassword(auth, email, password)
     };
+
+
+    onAuthStateChanged(auth, currentUser => {
+        if (currentUser) {
+            console.log('User Logged In', currentUser);
+            setUser(currentUser);
+        } else {
+            console.log('User Logged Out');
+            setUser(null);
+        }
+    });
+
+
     
     const authInfo = {
         name,
         // other data
+        User,
         createUser,
         signInUser 
     }
