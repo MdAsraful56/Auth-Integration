80b3da48e4fc322b4a8dcc73f0613c3d62946a95
MdAsraful56 <work.mdasraful56@gmail.com>
2025-02-25 07:58:39 +0600

    order component

---
+++ src/components/Orders/Orders.jsx
@@ -0,0 +1,22 @@
+
+
+const Orders = () => {
+    return (
+        <div>
+            <ul className="flex flex-wrap gap-3 ">
+                <li className="w-10 h-10 bg-blue-400">Order - 1</li>
+                <li className="w-10 h-10 bg-red-500">Order - 2</li>
+                <li className="w-10 h-10 bg-green-400">Order - 3</li>
+                <li className="w-10 h-10 bg-orange-500">Order - 4</li>
+                <li className="w-10 h-10 bg-red-200">Order - 5</li>
+                <li className="w-10 h-10 bg-black">Order - 6</li>
+                <li className="w-10 h-10 bg-blue-50">Order - 7</li>
+                <li className="w-10 h-10 bg-orange-950">Order - 8</li>
+                <li className="w-10 h-10 bg-green-50">Order - 9</li>
+                <li className="w-10 h-10 bg-green-900">Order - 10</li>
+            </ul>
+        </div>
+    );
+};
+
+export default Orders;
